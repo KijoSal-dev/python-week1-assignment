6dcc295d342ad663a4d72d59926193165c7de996
KijoSal-dev <sallykijo@gmail.com>
2024-11-01 11:55:38 +0300

    add user input

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask for user name
+name = input("What is your name?")
+
+# Ask for the user age
+age = input("How old are you?")
+
+# Ask for the user location
+location = input("Where do you live?")
+
+# Print personalized message 
+print('Hello,' + name + str(age) + "years old" + "live in" + location )
